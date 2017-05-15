292b0df97cd33f6cce58d5cbc19cf429f852ee90
DeividZavala <crusedmoss@gmail.com>
2017-05-15 15:06:02 -0500

    finished

--- buildScript/buildServer.js
+++ buildScript/buildServer.js
@@ -15,14 +15,6 @@ app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
-app.get('/users', function (req, res) {
-	res.json([
-		{"id": 1, "firstName": "David", "lastName": "Zavala", "email": "david@fixter.org"},
-		{"id": 2, "firstName": "Martin", "lastName": "Melo", "email": "martin@fixter.org"},
-		{"id": 3, "firstName": "Hector", "lastName": "BlisS", "email": "contacto@fixter.org"}
-	]);
-});
-
 app.listen(port, function (err) {
 	if(err){
 		console.log(err)
